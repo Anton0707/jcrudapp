e5239350a3267934572ebdd22afb0b34ef0eae87
admin <admin@admins-MacBook-Pro.local>
2016-11-24 19:14:03 +0300

    Initial commit

---
+++ src/main/java/ru/anton/dao/UserDAO.java
@@ -0,0 +1,14 @@
+package ru.anton.dao;
+
+import ru.anton.entity.User;
+
+import java.util.List;
+
+public interface UserDAO {
+    public int createUser(User user);
+    public User updateUser(User user);
+    public void deleteUser(int id);
+    public List<User> getAllUsers();
+    public User getUser(int id);
+    public List<User> getAllUsers(String userName);
+}
